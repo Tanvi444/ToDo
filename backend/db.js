c83efea01c7f15795222095a632cc070f657c36c
Tanvi <tanvitomar592@gmail.com>
2023-08-06 23:15:55 +0530

    Changes

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const mongoURI = "mongodb://0.0.0.0:27017/testnote";
+const connectToMongo = () => {
+  mongoose.connect(mongoURI, () => {
+    console.log("Connected to Mongo Successfully");
+  });
+};
+
+module.exports = connectToMongo;
